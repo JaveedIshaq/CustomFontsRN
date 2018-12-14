316abb1986a76ae8e095d80ad157b994267fc025
javeedishaq <ishaqjaveed1@gmail.com>
2018-12-14 14:20:26 +0500

    custom added

---
+++ src/utils/Fonts.js
@@ -0,0 +1,8 @@
+export const Fonts =  {
+   intercomRobotoMedium : 'intercom_roboto_medium',
+   nunitoBold : 'nunito_bold',
+   nunitoLight : 'nunito_light',
+   nunitoRegular : 'nunito_regular',
+   MontserratBold: 'Montserrat-Bold',
+   MontserratBlack: 'MontserratBlack'
+}
